740143a24c330a84c92c91bda826a649fb89043f
Karoll94 <karollmartin64@gmail.com>
2023-06-29 19:15:22 -0500

    Tarea 1 del módulo 3 de React

---
+++ src/components/ContactForm/InputNumber/InputNumber.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import s from './InputNumber.module.css';
+
+const InputNumber = ({ number, onNumberChange}) => {
+    return <input
+      type="tel"
+      name="number"
+      pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+      title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+      required
+      placeholder='Enter number'
+      value={number}
+      onChange={onNumberChange}
+      className={s.input}
+    />
+}
+
+InputNumber.propTypes = {
+  number: PropTypes.string.isRequired
+}
+
+export default InputNumber;
